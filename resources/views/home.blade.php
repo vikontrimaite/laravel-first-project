f3931f8a6f5d0bdb8b6d1c6ccf7d9fdff1fbe7d5
Vilija Kontrimaitė <vikontrimaite@gmail.com>
2021-04-19 20:47:39 +0300

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,24 @@
 @section('content')
 <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
-        HOME
+        <div>
+            <h1 class="font-bold mb-2">HOME</h1>
+            <p>Welcome to Posts page!</p>
+            <p>This is a website created with Laravel where only registered users can make posts!</p>
+        </div>
+        <div class="mt-5">
+            <p class="font-bold">
+                Since you are here already, go ahead and explore it:
+            </p>
+            <ul>
+                <li>Register a new user</li>
+                <li>Login in to your account</li>
+                <li>Make a new post</li>
+                <li>Delete your post</li>
+            </ul>
+
+        </div>
+
     </div>
 </div>
 @endsection
